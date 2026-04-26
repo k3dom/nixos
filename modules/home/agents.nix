afa000dcbb0dfc109f2df06c2619e5f2bf29e50e
kedom <kedom@vmcall.net>
2026-04-26 15:14:23 +0200

    feat: add pi agent

--- modules/home/agents.nix
+++ modules/home/agents.nix
@@ -6,6 +6,7 @@
       settings = {
         share = "disabled";
         autoupdate = false;
+        experimental.quotaToast.enableToast = true;
         plugin = [
           # https://github.com/mohak34/opencode-notifier
           "@mohak34/opencode-notifier@0.2.3"
@@ -13,14 +14,14 @@
           "opencode-claude-auth@1.5.0"
           # https://github.com/slkiser/opencode-quota
           "@slkiser/opencode-quota@3.1.3"
-          # https://github.com/Tarquinen/oc-tps
-          "oc-tps@0.0.6"
         ];
       };
     };
     claude-code = {
       enable = true;
-      package = pkgs.llm-agents.claude-code.override { disableTelemetry = true; };
+      package = pkgs.llm-agents.claude-code.override {
+        disableTelemetry = true;
+      };
     };
   };
   xdg.configFile."opencode/opencode-notifier.json".text = builtins.toJSON {
@@ -29,6 +30,7 @@
   };
   home.packages = with pkgs; [
     opencode-desktop
+    llm-agents.pi
     llm-agents.ccusage
     llm-agents.ccusage-opencode
   ];
